876abb29e773c0ce7c94b85af24b03f2f7f2a26f
br.venkat <br.venkat@gmail.com>
2022-01-15 13:42:38 +1100

    Implement arnie quotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,36 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  let responses
+  const allRequests = urls.map(async (url) => httpGet(url))
+
+  try {
+    responses = await Promise.all(allRequests)
+  } catch(e) {
+    console.log("Error occured when recieving response ",e)
+    throw e
+  }
+
+  return processArnieResponse(responses)
 };
+
+const processArnieResponse = (responses) => responses.reduce((output, currentEntry) => {
+    let { body =  '{}'} = currentEntry
+    const parsedResponse = JSON.parse(body)
+
+    const responseData = parsedResponse && parsedResponse.message
+
+    if (currentEntry.status === 200) {
+      output.push({
+        'Arnie Quote': responseData
+      })
+    } else {
+      output.push({
+        'FAILURE': responseData
+      })
+    }
+    return output
+  }, [])
 
 module.exports = {
   getArnieQuotes,
